d988e3a8780ec1ade17a59bda60dcf4c78e5d464
Victor Fernandez <manuel1508vc@gmail.com>
2024-03-10 12:10:35 -0500

    segundo commit

--- colegio/urls.py
+++ colegio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ControlInicial
+from .views import ControlInicial,CursosContorl
 
 urlpatterns = [
-    path('inicio/', view=ControlInicial)
+    path('inicio/', view=ControlInicial),
+    path('cursos/', view=CursosContorl.as_view())
 ] 
--- colegio/views.py
+++ colegio/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Curso, Usuario
+from .serializers import CursoSerializer
+from rest_framework import status
 
 
 
@@ -11,4 +13,27 @@ def ControlInicial(request):
         'message':'Bienvenido a mi APi de Colegio'
     })
 
-
+class CursosContorl(APIView):
+    def get(self, request):
+        resultado = Curso.objects.all()
+        print(resultado)
+        serializador = CursoSerializer(instance=resultado, many=True)
+        return Response(data={
+            'message':'Me hicieron get',
+            'content':serializador.data
+        })
+    def post(self,request):
+        print(request.data)
+        serializador = CursoSerializer(data=request.data)
+        validacion=serializador.is_valid()
+        if validacion:
+            serializador.save()
+            return Response(data={
+                'message':'Curso creado exitosamentre',
+                'content': ''
+            }, status=status.HTTP_201_CREATED )
+        else:
+            return Response(data={
+                'menssage':'Error al crear el curso',
+                'content': serializador.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
